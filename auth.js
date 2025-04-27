39e94647c2a7712d173c71f9aa9db76160837ed2
tanvishagoel <tanvishagoel03@gmail.com>
2025-04-27 22:57:18 +0530

    new

--- auth.js
+++ auth.js
@@ -50,5 +50,5 @@ app.get('/callback', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at https://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
